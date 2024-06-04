1b058fa30c0b40976f55ba74e51f7e6010106063
Vladyslav Yachyn <reqvite2233@gmail.com>
2024-06-04 18:45:23 +0300

    update settings

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  url: env("APP_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
+  url: env("RENDER_EXTERNAL_URL"),
+  dirs: {
+    public: "/data/public"
   },
 });
